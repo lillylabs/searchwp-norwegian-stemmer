55360554b5bb643b92b8cd278503d264a02c4671
Tom Erik Støwer <testower@gmail.com>
2014-05-03 21:43:27 +0200

    first commit

---
+++ test/NorwegianStemmerUnitTests.php
@@ -0,0 +1,24 @@
+<?php
+
+require('src/NorwegianStemmer.php');
+
+class NorwegianStemmerUnitTests extends PHPUnit_Framework_TestCase
+{
+    public function testStemming()
+    {
+        $handle = fopen("test/fixtures.txt", "r");
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
+                $words = preg_split('/\s+/', trim($line));
+                $this->assertEquals($words[1], NorwegianStemmer::Stem($words[0]));
+            }
+        } else {
+            echo 'Error opening the file';
+            return -1;
+        }
+
+        fclose($handle);
+    }
+}
+
+?>
